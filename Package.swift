12b1491a49bb80e1cdcc67ab35d89a6385ada769
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-02-18 15:38:16 +0300

    Publish swift package YandexMapsMobile version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobileLite",
+    defaultLocalization: "en",
+    platforms: [.iOS("12.0")],
+    products: [
+        .library(
+            name: "YandexMapsMobileLite",
+            type: .static,
+            targets: [
+                "YandexMapsMobileLite",
+                "YandexMapsMobileLiteResources"
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobileLite",
+            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.11.0-lite.framework.zip",
+            checksum: "8771c828dd6844b947d64e72a99140f7209fce99a0cd2eb607e728ddf74c408d"
+        ),
+        .target(
+            name: "YandexMapsMobileLiteResources",
+            path: "Resources",
+            linkerSettings: [
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("OpenGLES"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("Security"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("DeviceCheck"),
+                .linkedLibrary("resolv"),
+                .linkedLibrary("c++"),
+            ]
+
+        )
+    ]
+)
